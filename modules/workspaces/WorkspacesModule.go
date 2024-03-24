d4356c80e3a284d5878ee4f916d66030069ce3b2
Ali Torabi <ali-torabian@outlook.com>
2024-03-24 22:25:39 +0100

    App menu sync seeders

--- modules/workspaces/WorkspacesModule.go
+++ modules/workspaces/WorkspacesModule.go
@@ -56,6 +56,7 @@ func WorkspaceModuleSetup() *ModuleProvider {
 
 	module.ProvideSeederImportHandler(func() {
 		RegionalContentSyncSeeders()
+		AppMenuSyncSeeders()
 	})
 
 	module.ProvideMockImportHandler(func() {
