15baebdc0d557f6f465ca0e6341f1937fc49d928
Ali Torabi <ali-torabian@outlook.com>
2024-04-29 11:58:22 +0200

    Add the fireback spring boot support and boilerplate

--- otherside/fireback-spring/src/main/java/com/torabian/fireback/firebackspring/FirebackSpringApplication.java
+++ otherside/fireback-spring/src/main/java/com/torabian/fireback/firebackspring/FirebackSpringApplication.java
@@ -1,28 +1,13 @@
 package com.torabian.fireback.firebackspring;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.torabian.fireback.project.Book;
-import com.torabian.fireback.project.BookRepository;
-
+ 
 @SpringBootApplication
 public class FirebackSpringApplication  {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FirebackSpringApplication.class, args);
-		System.out.println("Hi this is a new spring boot application");
-
-		int[] numbers = {1,2,3,4,5,6,7,8};
-
-		BinarySearch n = new BinarySearch();
-
-		System.out.println(n.binarySearch(numbers, 5));
-		System.out.println("Finding books:");
-
-	}
  
-
+	public static void main(String[] args) {
+		SpringApplication.run(FirebackSpringApplication.class, args); 
+	}
 }
--- otherside/fireback-spring/src/main/java/com/torabian/fireback/project/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.torabian.fireback.project;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
