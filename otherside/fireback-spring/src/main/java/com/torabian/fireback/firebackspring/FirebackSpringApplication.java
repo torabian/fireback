2971c8d4f946fa1a6c17dc11b99884fad52005c6
Ali Torabi <torabian@users.noreply.github.com>
2024-04-29 12:02:38 +0200

    Fireback spring (#10)
    * Add spring boot
    
    * Add the fireback spring boot support and boilerplate

---
+++ otherside/fireback-spring/src/main/java/com/torabian/fireback/firebackspring/FirebackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.torabian.fireback.firebackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ 
+@SpringBootApplication
+public class FirebackSpringApplication  {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(FirebackSpringApplication.class, args); 
+	}
+}
