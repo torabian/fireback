74738d869ec0a7a60ae0e6fef181ffb2c4f55ac4
Ali Torabi <torabian@users.noreply.github.com>
2024-05-26 17:17:02 +0200

    Remove unused elements of the product (#38)

--- modules/cms/CmsModule.dyno.go
+++
@@ -1,9 +0,0 @@
-package cms
-import "github.com/torabian/fireback/modules/workspaces"
-// Module dynamic things comes here. Don't touch it :D
-var PERM_ROOT_CMS_EVERYTHING = workspaces.PermissionInfo{
-  CompleteKey: "root/cms/*",
-}
-var ALL_PERM_CMS_MODULE = []workspaces.PermissionInfo{
-  PERM_ROOT_CMS_EVERYTHING,
-}
--- modules/shop/ShopConstants.go
+++
@@ -1,7 +0,0 @@
-package shop
-
-var ORDER_STATUS_DONE = "done"
-var ORDER_STATUS_PENDING = "pending"
-
-var PAYMENT_STATUS_DONE = "done"
-var PAYMENT_STATUS_PENDING = "pending"
