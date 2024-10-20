2e240939cf9a59ba771747220a59caed76ca236e
Ali <ali-torabian@outlook.com>
2024-10-20 17:13:54 +0200

    Log the create

--- modules/workspaces/WorkspaceEntity.dyno.go
+++ modules/workspaces/WorkspaceEntity.dyno.go
@@ -9,6 +9,9 @@ import (
 	"embed"
 	"encoding/json"
 	"fmt"
+	reflect "reflect"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gookit/event"
 	jsoniter "github.com/json-iterator/go"
@@ -21,8 +24,6 @@ import (
 	"gopkg.in/yaml.v2"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
-	reflect "reflect"
-	"strings"
 )
 
 var workspaceSeedersFs = &seeders.ViewsFs
@@ -286,11 +287,13 @@ func WorkspaceRecursiveAddUniqueId(dto *WorkspaceEntity, query QueryDSL) {
 
 /*
 *
-	Batch inserts, do not have all features that create
-	operation does. Use it with unnormalized content,
-	or read the source code carefully.
-  This is not marked as an action, because it should not be available publicly
-  at this moment.
+
+		Batch inserts, do not have all features that create
+		operation does. Use it with unnormalized content,
+		or read the source code carefully.
+	  This is not marked as an action, because it should not be available publicly
+	  at this moment.
+
 *
 */
 func WorkspaceMultiInsert(dtos []*WorkspaceEntity, query QueryDSL) ([]*WorkspaceEntity, *IError) {
@@ -355,8 +358,11 @@ func WorkspaceActionCreateFn(dto *WorkspaceEntity, query QueryDSL) (*WorkspaceEn
 	query.Tx = dbref
 	err := dbref.Create(&dto).Error
 	if err != nil {
+		fmt.Println("111111 - error happened on creation", err)
 		err := GormErrorToIError(err)
 		return dto, err
+	} else {
+		fmt.Println("22222 - no error on creation")
 	}
 	// 5. Create sub entities, objects or arrays, association to other entities
 	WorkspaceAssociationCreate(dto, query)
