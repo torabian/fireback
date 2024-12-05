1d3af31f86c68da2448e330c877ab49ecd68b4f4
Ali <ali-torabian@outlook.com>
2024-12-05 16:54:11 +0100

    Fix the path

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://torabi.io/fireback",
+  url: "https://torabi.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/fireback/docs/",
