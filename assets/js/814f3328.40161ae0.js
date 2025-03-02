2e5638b201c4aee2fbb10b3aa692b1ae45503e18
torabian <torabian@users.noreply.github.com>
2025-03-02 18:33:42 +0000

    Deploying to gh-pages from @ torabian/fireback@0d4a76ad5349dda75fc4657d74e6120f6b4540a5 🚀

---
+++ assets/js/814f3328.40161ae0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[7472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"What is Fireback?","permalink":"/fireback/blog/what-is-fireback","unlisted":false,"date":"2025-03-02T18:25:38.000Z"}]}')}}]);
