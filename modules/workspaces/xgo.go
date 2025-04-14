cdd36be13bd7f5e0c7dac85a5ea2d987def5e097
Ali Torabi <ali-torabian@outlook.com>
2025-04-14 10:38:41 +0200

    Added swagger as well

--- modules/workspaces/xgo.go
+++ modules/workspaces/xgo.go
@@ -353,6 +353,31 @@ func SetupHttpServer(x *FirebackApp, cfg HttpServerInstanceConfig) *gin.Engine {
 		`)
 	})
 
+	r.GET("/swagger", func(c *gin.Context) {
+
+		c.Header("content-type", "text/html")
+		c.String(200, `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Swagger UI</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+
+    <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+    <script>
+      SwaggerUIBundle({
+        url: "/openapi.yml",
+        dom_id: '#swagger-ui'
+      });
+    </script>
+  </body>
+</html>
+
+		`)
+	})
+
 	r.GET("/openapi.yml", func(c *gin.Context) {
 		data, err := ConvertStructToOpenAPIYaml(x)
 
