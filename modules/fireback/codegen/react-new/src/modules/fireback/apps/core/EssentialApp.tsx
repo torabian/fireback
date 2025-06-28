b25af4fc098659bd562e6911c297dc88eb350efc
Ali Torabi <torabian@users.noreply.github.com>
2025-06-28 15:45:22 +0200

    Delete the android folder from capacitor project (#161)
    Co-authored-by: Ali <Ali>

--- modules/fireback/codegen/react-new/src/modules/fireback/apps/core/EssentialApp.tsx
+++ modules/fireback/codegen/react-new/src/modules/fireback/apps/core/EssentialApp.tsx
@@ -1,5 +1,3 @@
-import { ErrorBoundary } from "react-error-boundary";
-
 import "react-toastify/dist/ReactToastify.css";
 import "../../../../modules/fireback/styles/styles.scss";
 // You do not have to use the mac-os family theme at all.
@@ -8,18 +6,12 @@ import "../../../../modules/fireback/styles/styles.scss";
 // themes are nothing special, rather than wrapping a set of css (scss) on a global name
 import "../../../..//modules/fireback/styles/apple-family/styles.scss";
 
-import { AuthProvider } from "../../hooks/authContext";
 import { UIStateProvider } from "../../hooks/uiStateContext";
 
 import React, { useContext, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { Fallback } from "../../components/fallback/Fallback";
 
-import { ToastContainer } from "react-toastify";
-import { AppConfig, AppConfigContext } from "../../hooks/appConfigTools";
-import { usePureLocale } from "../../hooks/usePureLocale";
-import { WithFireback } from "./WithFireback";
-import { ApplicationPanels } from "./ApplicationPanels";
+import { AppConfigContext } from "../../hooks/appConfigTools";
 import { AppTree } from "./AppTree";
 
 export function EssentialApp({
