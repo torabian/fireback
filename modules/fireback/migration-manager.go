afceb5b19f443915e1053e68a2f7b3b8391234c9
Ali <Ali>
2026-03-06 16:26:18 +0100

    U

--- modules/fireback/migration-manager.go
+++ modules/fireback/migration-manager.go
@@ -4,23 +4,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
+
+	"gorm.io/gorm/logger"
 )
 
 func ApplyMigration(xapp *FirebackApp, level int64) {
 	db := GetDbRef()
 
-	// newLogger := logger.New(
-	// 	log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-	// 	logger.Config{
-	// 		SlowThreshold:             time.Second,            // Slow SQL threshold
-	// 		LogLevel:                  logger.LogLevel(level), // Log level
-	// 		IgnoreRecordNotFoundError: true,                   // Ignore ErrRecordNotFound error for logger
-	// 		ParameterizedQueries:      true,                   // Don't include params in the SQL log
-	// 		Colorful:                  false,                  // Disable color
-	// 	},
-	// )
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,            // Slow SQL threshold
+			LogLevel:                  logger.LogLevel(level), // Log level
+			IgnoreRecordNotFoundError: true,                   // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,                   // Don't include params in the SQL log
+			Colorful:                  false,                  // Disable color
+		},
+	)
 
-	// db.Config.Logger = newLogger
+	db.Config.Logger = newLogger
 
 	originalDb, _ := db.DB()
 	for _, item := range xapp.Modules {
