0012abc469a1ef82ab5c93729fc84eda249031a0
anonymouse <anonymouse@h>
2026-03-27 03:09:33 +0100

    U

--- modules/abac/CheckPassportMethodsAction.dyno.go
+++ modules/abac/CheckPassportMethodsAction.dyno.go
@@ -104,12 +104,12 @@ func CastCheckPassportMethodsActionResFromCli(c emigo.CliCastable) CheckPassport
 type CheckPassportMethodsActionRes struct {
 	Email                bool   `json:"email" yaml:"email"`
 	Phone                bool   `json:"phone" yaml:"phone"`
-	Google               bool   `json:"google" yaml:"google"`
+	Google               bool   `yaml:"google" json:"google"`
 	Facebook             bool   `json:"facebook" yaml:"facebook"`
 	GoogleOAuthClientKey string `json:"googleOAuthClientKey" yaml:"googleOAuthClientKey"`
-	FacebookAppId        string `yaml:"facebookAppId" json:"facebookAppId"`
+	FacebookAppId        string `json:"facebookAppId" yaml:"facebookAppId"`
 	EnabledRecaptcha2    bool   `json:"enabledRecaptcha2" yaml:"enabledRecaptcha2"`
-	Recaptcha2ClientKey  string `yaml:"recaptcha2ClientKey" json:"recaptcha2ClientKey"`
+	Recaptcha2ClientKey  string `json:"recaptcha2ClientKey" yaml:"recaptcha2ClientKey"`
 }
 
 func (x *CheckPassportMethodsActionRes) Json() string {
