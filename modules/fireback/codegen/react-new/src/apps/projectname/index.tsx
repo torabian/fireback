abc619086bcb0285d80677006b278aaa3d305dd8
Ali <Ali@ali>
2026-04-01 13:44:21 +0200

    Some more progress in vite loading

---
+++ modules/fireback/codegen/react-new/src/apps/projectname/index.tsx
@@ -0,0 +1,43 @@
+// @ts-nocheck
+
+/**
+ * TARGET_APP is an environment variables that you can
+ * use for spliting app into a different product.
+ * when you import App.tsx from an specific project,
+ * only routes and modules defined there will be added to your
+ * pack. This is similar to C preprocessors idea.
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// #v-ifdef PROD
+alert(93)
+// #v-end
+
+/// #if TARGET_APP == 'projectname'
+import App from "./apps/projectname/App";
+
+/// #endif
+
+// /// #if TARGET_APP == 'designer'
+// import App from "./apps/designer/App";
+
+// /// #endif
+
+// /// #if TARGET_APP == 'self-service'
+// import App from "./apps/self-service/App";
+
+// /// #endif
+
+// @fireback-append-app
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- modules/fireback/codegen/react-new/vite.config.ts
+++ modules/fireback/codegen/react-new/vite.config.ts
@@ -1,30 +1,58 @@
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react({
-      exclude: [
-        'node_modules/**', // exclude everything else in node_modules
-      ],
-      babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
-          ['@babel/plugin-proposal-class-properties', { loose: true }],
-          ['@babel/plugin-transform-private-methods', { loose: true }],
-          ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+export default defineConfig(({ command, mode }) => {
+  console.log('Vite command:', command) // 'serve' or 'build'
+  console.log('Current mode:', mode)    // 'development', 'production', or custom mode
+
+  // Path to the folder
+  const bv = mode.split('/')
+  const variation = bv.pop()
+  const jsonFolderPath = resolve(__dirname, `./src/apps/${bv.join('/')}/build-variables/${variation}.json`)
+  let build_variables = {};
+
+  // Check if folder exists
+  if (fs.existsSync(jsonFolderPath)) {
+    console.log("Build variation selected:", jsonFolderPath)
+    build_variables = require(jsonFolderPath)
+  } else {
+    console.log("Build variation does not exists:", jsonFolderPath)
+  }
+
+  return {
+    define: {
+      BUILD_VARIABLES: build_variables,
+      __BUILD_VARIABLES__: build_variables
+    },
+    plugins: [
+      tsconfigPaths(),
+      react({
+        exclude: [
+          'node_modules/**', // exclude everything else in node_modules
         ],
-      },
-    }),
-  ],
- 
-  resolve: {
-    alias: [
-      { find: "@/", replacement: resolve(__dirname, "./src/") }
-    ]
+        babel: {
+          plugins: [
+            ['@babel/plugin-proposal-decorators', { version: 'legacy' }],
+            ['@babel/plugin-proposal-class-properties', { loose: true }],
+            ['@babel/plugin-transform-private-methods', { loose: true }],
+            ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+          ],
+        },
+      }),
+    ],
+    resolve: {
+      alias: [
+        { find: "@/", replacement: resolve(__dirname, "./src/") }
+      ]
+    },
+    // Optional: change envDir based on mode
+    envDir: mode === 'staging' ? './config/staging-envs' : './config/env',
   }
 })
