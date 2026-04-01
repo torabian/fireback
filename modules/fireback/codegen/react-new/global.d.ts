de6b127c9c7737e8542a0966d1dd36ab733975c1
Ali <Ali@ali>
2026-04-01 18:48:01 +0200

    Add the definitions

--- modules/fireback/codegen/react-new/global.d.ts
+++ modules/fireback/codegen/react-new/global.d.ts
@@ -1,2 +1,20 @@
 declare module "*.css";
 declare module "*.scss";
+
+interface Window {
+    BUILD_VARIABLES?: {
+        REMOTE_SERVICE?: string;
+        PUBLIC_URL?: string;
+        DEFAULT_ROUTE?: string;
+        TARGET_APP?: string;
+        ALLOW_OS_LOGIN?: string;
+        SUPPORTED_LANGUAGES?: string;
+        USE_HASH_ROUTER?: string;
+        INACCURATE_MOCK_MODE?: string;
+        TITLE?: string;
+        GITHUB_DEMO?: string;
+        FORCED_LOCALE?: string;
+        FORCE_APP_THEME?: string;
+        NAVIGATE_ON_SIGNOUT?: string;
+    };
+}
