ef59c8fce02f6f651b05270f9049c52f0d0513d8
anonymouse <anonymouse@h>
2026-04-07 02:31:03 +0200

    Update

--- modules/fireback/FirebackCustomActions.dyno.go
+++ modules/fireback/FirebackCustomActions.dyno.go
@@ -6,15 +6,16 @@ package fireback
 *	Checkout the repository for licenses and contribution: https://github.com/torabian/fireback
  */
 import (
-	"reflect"
-
 	"github.com/gin-gonic/gin"
 	"github.com/urfave/cli"
 )
+import (
+	"reflect"
+)
 
 // using shared actions here
-// / For emi, we also need to print the handlers, and also print security model, which is a part of Fireback
-// / and not available in Emi (won't be)
+/// For emi, we also need to print the handlers, and also print security model, which is a part of Fireback
+/// and not available in Emi (won't be)
 var EventBusSubscriptionSecurityModel = &SecurityModel{
 	ActionRequires:  []PermissionInfo{},
 	ResolveStrategy: "workspace",
