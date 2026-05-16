2a70399f3896322c7a36b42c60975b99be0c78a7
Ali <Ali@ali>
2026-05-16 18:38:19 +0200

    Update XDateTimeType.go

--- modules/fireback/XDateTimeType.go
+++ modules/fireback/XDateTimeType.go
@@ -13,6 +13,7 @@ import (
 	ptime "github.com/yaa110/go-persian-calendar"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
 )
 
 type XDateTime string
@@ -158,13 +159,24 @@ func (date XDateTime) Value() (driver.Value, error) {
 	if date == "" {
 		return nil, nil
 	}
-	return string(date), nil
 
+	t, err := time.Parse(time.RFC3339, string(date))
+	if err != nil {
+		return nil, err
+	}
+
+	return t, nil
 }
 
-// GormDataType gorm common data type
-func (date XDateTime) GormDataType() string {
-	return "datetime"
+func (date XDateTime) GormDBDataType(db *gorm.DB, field *schema.Field) string {
+	switch db.Dialector.Name() {
+	case "postgres":
+		return "timestamptz"
+	case "mysql":
+		return "datetime"
+	default:
+		return "datetime"
+	}
 }
 
 func (js XDateTime) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
@@ -173,6 +185,12 @@ func (js XDateTime) GormValue(ctx context.Context, db *gorm.DB) clause.Expr {
 	}
 
 	switch db.Dialector.Name() {
+	case "postgres":
+		t, err := time.Parse(time.RFC3339, string(js))
+		if err == nil {
+			return gorm.Expr("?", t)
+		}
+
 	case "mysql":
 		parsedTime, err := time.Parse(time.RFC3339, string(js))
 		if err == nil {
