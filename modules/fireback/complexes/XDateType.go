87a80dde592ee9a7e47432ecaee29b9b29175f26
Ali <Ali@ali>
2026-08-04 16:27:08 +0200

    asdasd

--- modules/fireback/complexes/XDateType.go
+++ modules/fireback/complexes/XDateType.go
@@ -179,3 +179,15 @@ func (j *XDate) UnmarshalJSON(b []byte) error {
 
 	return nil
 }
+
+func FormatDateBasedOnQuery(nsec int64, lang string) string {
+
+	t := time.Unix(0, nsec)
+
+	if lang == "fa" {
+		pt := ptime.New(t)
+		return persian.ToPersianDigits(fmt.Sprint(pt.Day(), pt.Month(), pt.Year()))
+
+	}
+	return t.Local().Format("2006/01/02 15:04:05")
+}
