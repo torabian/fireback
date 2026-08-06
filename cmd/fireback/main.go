b1b13c20ff0f51bc528d4636393bca420a17c52f
Ali <Ali@ali>
2026-08-07 01:52:46 +0200

    Update main.go

--- cmd/fireback/main.go
+++ cmd/fireback/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"os"
 
-	"github.com/gin-gonic/gin"
-	// "github.com/torabian/fireback/modules/abac"
 	"github.com/torabian/emi/lib/gorunner"
 	"github.com/urfave/cli/v3"
 
@@ -12,6 +10,7 @@ import (
 	"github.com/torabian/fireback/modules/abac/interfacetools"
 	"github.com/torabian/fireback/modules/backup"
 	"github.com/torabian/fireback/modules/fireback"
+
 	// clitools registers every terminal/CLI-interactive fireback feature
 	// (promptui/bubbletea prompts, os/exec service management, asynq
 	// workers, graceful HTTP shutdown) via init(). It's tagged !wasm and
@@ -28,11 +27,6 @@ var PRODUCT_NAMESPACENAME = "fireback"
 var PRODUCT_DESCRIPTION = "Fireback core microservice - v" + fireback.FIREBACK_VERSION
 var PRODUCT_LANGUAGES = []string{"fa", "en"}
 
-// Fireback doesn't come with default ui in the cmd anymore.
-// Fireback itself has 2 uis: Manage and SelfService.
-// Developer needs to build them if necessary and put the static files in workspaces
-// Folder. Fireback serves them on /manage and /selfservice, similarly child projects
-// Can serve those react projects if they wanted to.
 // //go:embed all:ui
 // var ui embed.FS
 
@@ -84,23 +78,9 @@ func main() {
 			abac.PassportMethodSyncSeeders()
 			interfacetools.AppMenuSyncSeeders()
 		},
-
 		PublicFolders: []gintools.PublicFolderInfo{
-			// You can set a series of static folders to be served along with fireback.
-			// This is only for static content. For advanced MVX render templates, you need to
-			// Bootstrap those themes
-			// Add these two lines on the top of the file
-			/////go:embed all:ui
-			// var ui embed.FS
-			// and then uncomment this, for example to serve static react or angular content
-
-			// 		//go:embed all:selfservice
-			// var selfservice embed.FS
 			{Fs: &FBManage.FirebackManageTmpl, Folder: ".", Prefix: "/manage"},
 			{Fs: &FbSelfService.FbSelfService, Folder: ".", Prefix: "/selfservice"},
-		},
-		SetupWebServerHook: func(e *gin.Engine, xs *fireback.FirebackApp) {
-
 		},
 		Modules: modules,
 	}
