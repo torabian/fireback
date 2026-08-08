85aaac4c61f7bb7a162810549995621255c0319b
Ali <Ali@ali>
2026-08-08 18:04:34 +0200

    Effort to write tests for full functionality

--- e2e/cypress/e2e/login.cy.js
+++ e2e/cypress/e2e/login.cy.js
@@ -31,169 +31,169 @@ describe("Logging in with the signin", () => {
       cy.task("exec", ` passport method c --region global --type email`);
     });
 
-    it("should be able to create phone method in database.", () => {
-      cy.task("exec", ` passport method c --region global --type phone`);
-    });
+    // it("should be able to create phone method in database.", () => {
+    //   cy.task("exec", ` passport method c --region global --type phone`);
+    // });
 
-    it("get the data of the public", () => {
-      cy.request(
-        "GET",
-        "http://localhost:7794/passports/available-methods",
-      ).then((response) => {
-        cy.task("log", response.body);
-        expect(response.body.data.item.email).to.equal(true);
-        expect(response.body.data.item.phone).to.equal(true);
-      });
-    });
+    // it("get the data of the public", () => {
+    //   cy.request(
+    //     "GET",
+    //     "http://localhost:7794/passports/available-methods",
+    //   ).then((response) => {
+    //     cy.task("log", response.body);
+    //     expect(response.body.data.item.email).to.equal(true);
+    //     expect(response.body.data.item.phone).to.equal(true);
+    //   });
+    // });
 
-    it("should show welcome back when it's email and phone enabled.", () => {
-      cy.viewport(400, 750); // Set the window size dynamically
-      cy.visit(ui("/manage/#/en/welcome"));
-      cy.wait(1000);
-      cy.get("h1").should("have.text", "Welcome back");
-    });
+    // it("should show welcome back when it's email and phone enabled.", () => {
+    //   cy.viewport(400, 750); // Set the window size dynamically
+    //   cy.visit(ui("/manage/#/en/welcome"));
+    //   cy.wait(1000);
+    //   cy.get("h1").should("have.text", "Welcome back");
+    // });
 
-    it("on creation of the passport method, both type and region need to be provided.", () => {
-      cy.task("execSupress", ` passport method c`).then((content) => {
-        expect(content).contain('"type, region"');
-      }).ca;
-    });
+    // it("on creation of the passport method, both type and region need to be provided.", () => {
+    //   cy.task("execSupress", ` passport method c`).then((content) => {
+    //     expect(content).contain('"type, region"');
+    //   }).ca;
+    // });
 
-    let roleId = "";
-    it("should be able to create a role in order to assign it into the workspace type.", () => {
-      cy.task(
-        "exec",
-        ` role c --name testagentrole --capabilities "root.*"`,
-      ).then((res) => {
-        console.log("Role created:", res)
-        console.log((roleId = JSON.parse(res).uniqueId));
-      });
-    });
+    // let roleId = "";
+    // it("should be able to create a role in order to assign it into the workspace type.", () => {
+    //   cy.task(
+    //     "exec",
+    //     ` role c --name testagentrole --capabilities "root.*"`,
+    //   ).then((res) => {
+    //     console.log("Role created:", res)
+    //     console.log((roleId = JSON.parse(res).uniqueId));
+    //   });
+    // });
 
-    it("should be able to create a workspace name", () => {
-      cy.task(
-        "exec",
-        ` ws type c --title customer --slug customer --role-id ${roleId}`,
-      );
-    });
+    // it("should be able to create a workspace name", () => {
+    //   cy.task(
+    //     "exec",
+    //     ` ws type c --title customer --slug customer --role-id ${roleId}`,
+    //   );
+    // });
 
-    it("should be able to create an account", () => {
-      Cypress.on("uncaught:exception", (err, runnable) => {
-        // returning false here prevents Cypress from
-        // failing the test
-        return false;
-      });
+    // it("should be able to create an account", () => {
+    //   Cypress.on("uncaught:exception", (err, runnable) => {
+    //     // returning false here prevents Cypress from
+    //     // failing the test
+    //     return false;
+    //   });
 
-      cy.viewport(400, 750); // Set the window size dynamically
+    //   cy.viewport(400, 750); // Set the window size dynamically
 
-      cy.visit(ui("/manage/#/en/welcome"));
-      cy.get("#using-email").should("exist").click();
-      cy.url().should("include", "/selfservice/email");
-      cy.get("h1").should("have.text", "Continue with Email");
-      cy.wait(1000);
+    //   cy.visit(ui("/manage/#/en/welcome"));
+    //   cy.get("#using-email").should("exist").click();
+    //   cy.url().should("include", "/selfservice/email");
+    //   cy.get("h1").should("have.text", "Continue with Email");
+    //   cy.wait(1000);
 
-      // // Check if the go back works just fine.
-      cy.get("#go-back-button").should("exist").click({ force: true });
-      cy.url().should("match", /\/welcome$/);
+    //   // // Check if the go back works just fine.
+    //   cy.get("#go-back-button").should("exist").click({ force: true });
+    //   cy.url().should("match", /\/welcome$/);
 
-      cy.get("#using-email").should("exist").as("btn").click({ force: true });
-      cy.get("#value-input").type("test@test.com"); // Fill the input with "admin"
-      cy.wait(500);
+    //   cy.get("#using-email").should("exist").as("btn").click({ force: true });
+    //   cy.get("#value-input").type("test@test.com"); // Fill the input with "admin"
+    //   cy.wait(500);
 
-      cy.get("#submit-form").click({ force: true }); // Submit the form
+    //   cy.get("#submit-form").click({ force: true }); // Submit the form
 
-      cy.wait(500);
+    //   cy.wait(500);
 
-      cy.get("h1").should("have.text", "Complete your account");
+    //   cy.get("h1").should("have.text", "Complete your account");
 
-      cy.get("#first-name-input").type("Ali");
-      cy.get("#last-name-input").type("Torabi");
+    //   cy.get("#first-name-input").type("Ali");
+    //   cy.get("#last-name-input").type("Torabi");
 
-      cy.get("#password-input").type("123321");
+    //   cy.get("#password-input").type("123321");
 
-      cy.get("#submit-form").click({ force: true }); // Submit the form
+    //   cy.get("#submit-form").click({ force: true }); // Submit the form
 
-      cy.wait(500);
-    });
+    //   cy.wait(500);
+    // });
 
-    let successfulInserts = 0;
-    let appMenuItems = [];
+    // let successfulInserts = 0;
+    // let appMenuItems = [];
 
-    describe("Login with the email address needs to be working", () => {
-      it("should be able to wipe the menu items", () => {
-        cy.task("exec", `misc appmenu wipe`).then((content) => {
-          expect(content).to.contain("of entities");
-        });
-      });
+    // describe("Login with the email address needs to be working", () => {
+    //   it("should be able to wipe the menu items", () => {
+    //     cy.task("exec", `misc appmenu wipe`).then((content) => {
+    //       expect(content).to.contain("of entities");
+    //     });
+    //   });
 
-      it("should be able to generate back the menu items from seeder.", () => {
-        cy.task("exec", `misc appmenu ssync`).then((content) => {
-          console.log(100, content);
-          let countFilesImported = 0;
-          for (const line of content.split("\n")) {
-            if (line.startsWith("Success")) {
-              successfulInserts += +line.match(/Success (\d+)/)[1];
-              expect(line).to.contain("Failure 0");
-            }
-            if (line.endsWith(".yml")) {
-              countFilesImported++;
-            }
-          }
+    //   it("should be able to generate back the menu items from seeder.", () => {
+    //     cy.task("exec", `misc appmenu ssync`).then((content) => {
+    //       console.log(100, content);
+    //       let countFilesImported = 0;
+    //       for (const line of content.split("\n")) {
+    //         if (line.startsWith("Success")) {
+    //           successfulInserts += +line.match(/Success (\d+)/)[1];
+    //           expect(line).to.contain("Failure 0");
+    //         }
+    //         if (line.endsWith(".yml")) {
+    //           countFilesImported++;
+    //         }
+    //       }
 
-          expect(countFilesImported).to.equal(3);
-        });
-      });
+    //       expect(countFilesImported).to.equal(3);
+    //     });
+    //   });
 
-      it("should be able to query the created content, and total count of data in database are equal to what we have imported", () => {
-        cy.task("exec", `misc appmenu q`).then((content) => {
-          const res = JSON.parse(content);
-          expect(res.data.totalItems).to.equal(successfulInserts);
-          appMenuItems = res.data.items;
-        });
-      });
+    //   it("should be able to query the created content, and total count of data in database are equal to what we have imported", () => {
+    //     cy.task("exec", `misc appmenu q`).then((content) => {
+    //       const res = JSON.parse(content);
+    //       expect(res.data.totalItems).to.equal(successfulInserts);
+    //       appMenuItems = res.data.items;
+    //     });
+    //   });
 
-      describe("testing the menu items content", () => {
-        it("all menu items, should have visibility of A", () => {
-          validateAppMenuEntity(appMenuItems);
-        });
-      });
+    //   describe("testing the menu items content", () => {
+    //     it("all menu items, should have visibility of A", () => {
+    //       validateAppMenuEntity(appMenuItems);
+    //     });
+    //   });
 
-      describe("cte operations", () => {
-        it("should be able to query as a tree structure the menu items", () => {
-          cy.task("exec", `misc appmenu cte`).then((content) => {
-            console.log(content);
-            const res = JSON.parse(content);
+    //   describe("cte operations", () => {
+    //     it("should be able to query as a tree structure the menu items", () => {
+    //       cy.task("exec", `misc appmenu cte`).then((content) => {
+    //         console.log(content);
+    //         const res = JSON.parse(content);
 
-            // general validation
-            validateAppMenuEntity(res.data.items);
+    //         // general validation
+    //         validateAppMenuEntity(res.data.items);
 
-            // There should be 3 items because there are 3 root items
-            expect(res.data.totalItems).to.equal(3);
+    //         // There should be 3 items because there are 3 root items
+    //         expect(res.data.totalItems).to.equal(3);
 
-            // test if items are having children with more than 1 item in them.
-            for (const item of res.data.items) {
-              if (item.children && item.children.length > 0) {
-                expect(item.children?.length).to.be.greaterThan(0);
-              }
-            }
-          });
-        });
-      });
+    //         // test if items are having children with more than 1 item in them.
+    //         for (const item of res.data.items) {
+    //           if (item.children && item.children.length > 0) {
+    //             expect(item.children?.length).to.be.greaterThan(0);
+    //           }
+    //         }
+    //       });
+    //     });
+    //   });
 
-      describe("operate on the single menu item", () => {
-        it("running update on the entity, should only affected the updated time.", () => {
-          const item = appMenuItems[0];
+    //   describe("operate on the single menu item", () => {
+    //     it("running update on the entity, should only affected the updated time.", () => {
+    //       const item = appMenuItems[0];
 
-          cy.task(
-            "exec",
-            `misc appmenu q --query "unique_id = ${item.uniqueId}"`,
-          ).then((content) => {
-            const res = JSON.parse(content);
-            console.log(res);
-          });
-        });
-      });
-    });
+    //       cy.task(
+    //         "exec",
+    //         `misc appmenu q --query "unique_id = ${item.uniqueId}"`,
+    //       ).then((content) => {
+    //         const res = JSON.parse(content);
+    //         console.log(res);
+    //       });
+    //     });
+    //   });
+    // });
 
     endFirebackServer();
   });
