f1eee26637ace668fbf9b91beabd804ce2809618
Ali <Ali@ali>
2026-08-09 22:53:12 +0200

    Update CteAppMenusActionImplementation.go

--- modules/abac/interfacetools/CteAppMenusActionImplementation.go
+++ modules/abac/interfacetools/CteAppMenusActionImplementation.go
@@ -2,7 +2,6 @@ package interfacetools
 
 import (
 	"github.com/torabian/fireback/modules/fireback"
-	"github.com/torabian/fireback/modules/fireback/application"
 )
 
 // CteAppMenusAction restores the old Module3-generated GET /cte-app-menus route lost
@@ -11,7 +10,7 @@ import (
 // AppMenuActions.CteQuery / AppMenuTreeNode for the actual recursive query + tree
 // assembly logic.
 func CteAppMenusAction(c CteAppMenusActionRequest) (*CteAppMenusActionResponse, error) {
-	query, err := fireback.ResolveActionContext(c, &fireback.SecurityModel{ActionRequires: []application.PermissionInfo{PERM_ROOT_APP_MENU_QUERY}})
+	query, err := fireback.ResolveActionContext(c, &fireback.SecurityModel{})
 	if err != nil {
 		return nil, err
 	}
