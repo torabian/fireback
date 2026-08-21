96c0fa5cd8991983858e879699edcc3de53a00d1
Ali <Ali@ali>
2026-08-21 03:37:12 +0200

    Update QueryDSL.go

--- modules/abac/resolve/QueryDSL.go
+++ modules/abac/resolve/QueryDSL.go
@@ -3,71 +3,16 @@ package resolve
 import (
 	"encoding/json"
 
-	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/torabian/fireback/modules/fireback/application"
-	"github.com/urfave/cli/v3"
-	"gorm.io/gorm"
 )
 
 type QueryDSL struct {
-
-	// It's gonna make left joins on the query, if the entity has
-	// objects or arrays. It would slow down the query dramatically.
-	Deep bool `json:"deep"`
-
-	// It would indicate how the result to be sorted on SQL queries
-	Sort string `json:"sort"`
-
-	// Useful when querying against a single element, and by passing
-	// uniqueId you can retrieve the record
-	UniqueId string `json:"uniqueId"`
-
-	// Sometimes you need to access the raw socket connection, here there is :)
-	RawSocketConnection *websocket.Conn
-
-	// It would indicate to the Gorm orm which tables to be included in the
-	// SQL search.
-	WithPreloads []string `json:"withPreloads"`
-
-	// this is gin context upon the request, which is being attached to the dsl
-	// regularly, should not be accessed directly but in reality many times we need
-	// to work low level and there is no reason framework do not allow it.
-	G *gin.Context `json:"-" yaml:"-"`
-
-	// this is cli context upon the request, which is being attached to the dsl
-	// regularly, should not be accessed directly but in reality many times we need
-	// to work low level and there is no reason framework do not allow it.
-	C *cli.Command `json:"-" yaml:"-"`
-
-	// The gorm transaction object. By setting the query Tx, you can connect
-	// few Fireback actions to be done as transaction. Fireback also uses this
-	// Object between it's own operations
-	Tx *gorm.DB
-
-	// This event will be trigged in the system, if that action is done
-	TriggerEventName string `json:"-"`
-
-	// The header authorization, the encrypted token is availble
-	// in every request
-	Authorization string `json:"authorization"`
 
 	// Automatically assigned UserId to the request after analising the token
 	// This will be used to save each entity and determine the owner of the record
 	UserId string `json:"-"`
 
 	ResolveStrategy string `json:"-"`
-
-	LinkerId string `json:"-"`
-
-	/// Extra where sql generated in the filter process.
-	FilterQuery string `json:"-" yaml:"-"`
-
-	/// JQ Json query generated
-	JqQuery string `json:"-" yaml:"-"`
-
-	// This is the person who is requesting, regardless of the workspace
-	SearchPhrase string `json:"searchPhrase"`
 
 	// This is the workspace which user is working inside, usually data belongs there
 	WorkspaceId string `json:"-"`
@@ -83,10 +28,8 @@ type QueryDSL struct {
 	// This is limitation of that workspace
 	WorkspaceHas []string `json:"-"`
 
-	InternalQuery string   `json:"-"`
-	Language      string   `json:"-"`
-	Region        string   `json:"-"`
-	Preloads      []string `json:"-"`
+	InternalQuery string `json:"-"`
+	Language      string `json:"-"`
 }
 
 func (x QueryDSL) Json() string {
